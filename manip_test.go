4ce6671fcc7680543de54ac21cf888e6d1ffae4c
Vorduin <vorduin@protonmail.com>
2022-03-01 02:45:45 +0200

    Disabled manip_test.go

--- manip_test.go
+++ manip_test.go
@@ -4,120 +4,120 @@
 
 package nune_test
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/vorduin/nune"
-)
+// 	"github.com/vorduin/nune"
+// )
 
-func BenchmarkCast1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1)
+// func BenchmarkCast1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1)
 
-	benchmarkMicro(b, func() {
-		nune.Cast[float32](tensor)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		nune.Cast[float32](tensor)
+// 	})
+// }
 
-func BenchmarkClone1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1)
+// func BenchmarkClone1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1)
 
-	benchmarkMicro(b, func() {
-		tensor.Clone()
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Clone()
+// 	})
+// }
 
-func BenchmarkReshape1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1)
+// func BenchmarkReshape1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1)
 
-	benchmarkMicro(b, func() {
-		tensor.Reshape(100, 200, 500)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Reshape(100, 200, 500)
+// 	})
+// }
 
-func BenchmarkIndex1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
+// func BenchmarkIndex1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Index(50)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Index(50)
+// 	})
+// }
 
-func BenchmarkSlice1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
+// func BenchmarkSlice1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Slice(25, 75)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Slice(25, 75)
+// 	})
+// }
 
-func BenchmarkBroadcast1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 200, 1).Reshape(200, 1)
+// func BenchmarkBroadcast1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 200, 1).Reshape(200, 1)
 
-	benchmarkMicro(b, func() {
-		tensor.Broadcast(10, 200, 500)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Broadcast(10, 200, 500)
+// 	})
+// }
 
-func BenchmarkReverse1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1)
+// func BenchmarkReverse1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1)
 
-	benchmarkMicro(b, func() {
-		tensor.Reverse()
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Reverse()
+// 	})
+// }
 
-func BenchmarkFlip1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
+// func BenchmarkFlip1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Flip(1)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Flip(1)
+// 	})
+// }
 
-func BenchmarkRepeat1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e5, 1).Reshape(200, 500)
+// func BenchmarkRepeat1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e5, 1).Reshape(200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Repeat(10)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Repeat(10)
+// 	})
+// }
 
-func BenchmarkPermute1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
+// func BenchmarkPermute1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Permute(2, 1, 0)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Permute(2, 1, 0)
+// 	})
+// }
 
-func BenchmarkCat1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 5e5, 1).Reshape(5, 200, 500)
+// func BenchmarkCat1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 5e5, 1).Reshape(5, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Cat(tensor, 0)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Cat(tensor, 0)
+// 	})
+// }
 
-func BenchmarkStack1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 5e5, 1).Reshape(5, 200, 500)
+// func BenchmarkStack1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 5e5, 1).Reshape(5, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Stack(tensor, 0)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Stack(tensor, 0)
+// 	})
+// }
 
-func BenchmarkSqueeze1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
+// func BenchmarkSqueeze1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Squeeze(1)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Squeeze(1)
+// 	})
+// }
 
-func BenchmarkUnsqueeze1e6(b *testing.B) {
-	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 1, 200, 500)
+// func BenchmarkUnsqueeze1e6(b *testing.B) {
+// 	tensor := nune.Range[float64](0, 1e6, 1).Reshape(10, 1, 200, 500)
 
-	benchmarkMicro(b, func() {
-		tensor.Unsqueeze(1)
-	})
-}
+// 	benchmarkMicro(b, func() {
+// 		tensor.Unsqueeze(1)
+// 	})
+// }
